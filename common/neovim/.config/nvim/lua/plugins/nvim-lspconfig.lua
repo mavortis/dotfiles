de299fbc8f8eab91cba70e0f0cfdf2316d04eae9
mars <marsvindex@gmail.com>
2025-02-19 15:56:30 -0800

    feat(nvim): add bash-ls

---
+++ common/neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh" },
+        },
+        lua_ls = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      },
+    },
+  },
+}
